8e4499d15664c1204bc36f6327857314caea20da
djmallek <djmallek1@gmail.com>
2016-03-09 00:01:39 -0600

    Add basic serial communication

--- GamepadTest.py
+++ GamepadTest.py
@@ -2,6 +2,8 @@ __author__ = 'Danny'
 
 import time
 import pygame
+import serial
+
 pygame.init()
 pygame.joystick.init()
 done = False
@@ -12,15 +14,31 @@ joystick.init()
 def getJoyVal( axis ):
     "this function returns the value of a joystick, given an axis and a joystick number"
     if abs(joystick.get_axis(axis))>0.2:
-        value = joystick.get_axis(axis)
+        Value = joystick.get_axis(axis)
     else:
-        value = 0.0
-    return value
+        Value = 0.0
+    return Value
 
-while done==False:
+def getButtonVal():
+    return 0
+
+
+ser = serial.Serial('COM5', 9600)  # open first serial port
+print ser.portstr       # check which port was really used
+ser.write("hello")      # write a string
+#ser.close()
+
+
+
+while 1:
     pygame.event.get()
 
 
-    print getJoyVal(0)
-    print getJoyVal(1)
-    time.sleep(.5)
+    x = getJoyVal(0)
+    y = getJoyVal(1)
+
+    ser.write("The value   :")
+    ser.write(str(x))
+    time.sleep(0.5)
+
+    #ser.close()
