4e691243eff9975c8a546987b713554116885f34
djmallek <djmallek1@gmail.com>
2016-04-17 18:30:24 -0500

    Create new way to parse serial stream

---
+++ ParserWithTypeCast.cpp
@@ -0,0 +1,15 @@
+/* strtof example */
+#include <stdio.h>      /* printf, NULL */
+#include <stdlib.h>     /* strtof */
+
+int main ()
+{
+  char szOrbits[] = "0.15 0.1756";
+  char* pEnd;
+  float f1, f2;
+  f1 = strtof (szOrbits, &pEnd);
+  f2 = strtof (pEnd, NULL);
+  printf("%f",f1);
+  printf("%f",f2);
+  return 0;
+}
